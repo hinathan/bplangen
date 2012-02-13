878b0d19a5672ee699e9c1c52b83794a259868f9
Nathan Schmidt <nathan@Meow.local>
2012-02-12 21:37:22 -0800

    replace photo with my own

--- public/index.php
+++ public/index.php
@@ -1,15 +1,19 @@
 <style>
 * {
 	font-family: Helvetica;
+	color:white;
+}
+body {
+	background-color:#40442D;
 }
 .top {
 	position: absolute;
-	top:75px;
-	left:180px;
-	width:450px;
-	height:450px;
+	top:120px;
+	left:270px;
+	max-width:500px;
+	max-height:500px;
 	z-index:2;
-/*	-webkit-transform:rotate(6deg);*/
+	-webkit-transform:rotate(3deg);
 	border:none;
 }
 .back {
@@ -22,7 +26,7 @@
 p {
 	position:absolute;
 	top:0px;
-	right:0px;
+	left:0px;
 	z-index:3;
 	padding:3px;
 }
@@ -30,6 +34,6 @@ p {
 <title>Business plan generator</title>
 <a href=/>
 <img src="/graph.php" class="top"/>
-<img src="/napkin.png" class="back" />
+<img src="/napkin.jpg" class="back" />
 </a>
-<p>A silly toy made by <a href="https://www.facebook.com/hinathan">Nathan Schmidt</a>.</p>
+<p>Business plan generator<br/><small>A silly toy made by <a href="https://www.facebook.com/hinathan">Nathan Schmidt</a>.</small></p>
